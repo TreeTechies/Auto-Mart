41c9ede74eb6d35b7e7ee03680ef9f11fa36d9ee
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2019-06-05 18:02:06 +0200

    Signup and Signin api

---
+++ server/middleware/validation.middleware.js
@@ -0,0 +1,30 @@
+//  Imports
+const Joi = require('@hapi/joi'); //  Import Joi for Validations
+
+//  Register validation
+function registerValidation(data) {
+    const userSchema = {
+        id:         Joi.number().required(),
+        email:      Joi.string().required().email(),
+        first_name: Joi.string().required(),
+        last_name:  Joi.string().required(),
+        password:   Joi.string().min(6).required(),
+        address:    Joi.string().required(),
+        is_admin:   Joi.boolean().required(),
+    };
+
+    return Joi.validate(data, userSchema);
+}
+
+//  Login validation
+function loginValidation(data) {
+    const schema = {
+        email:      Joi.string().required().email(),
+        password:   Joi.string().min(6).required()
+    };
+
+    return Joi.validate(data, schema);
+}
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
