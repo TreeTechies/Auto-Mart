bf0dd78ab13521d13f38fd4cfbfb11db9da1cb6d
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2019-06-14 00:56:17 +0200

    fix api

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use('/api/v1/users', user_route);
 app.use('/api/v1/cars', car_route);
 app.use('/api/v1/order', order_route);
 
-const server = app.listen(PORT);
+const server = app.listen(PORT, ()=>{
+    console.log(`running on port ${PORT}`)
+});
 
 module.exports = server;
