8880fb275d431b3281b675e704fbfa2950940fea
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2019-06-17 13:22:58 +0200

    Added babel

---
+++ server/db/script_queries.sql
@@ -0,0 +1,30 @@
+
+/* Creating Database */
+CREATE DATABASE auto_mart_db;
+
+/* Creating Table */
+CREATE TABLE [User](
+    [Id] varchar(255),
+    [FirstName] varchar(50),
+    [LastName] varchar(50),
+    [Email] varchar(50),
+    [Password] varchar(255),
+    [Address] varchar(50),
+    [IsAdmin] boolean
+);
+
+/* Inserting data to a table */
+INSERT INTO [User] VALUES();
+
+/* Reading data */
+SELECT * FROM table_name;
+
+/* Updating data */
+UPDATE table_name SET column1 = value1, ... WHERE condition;
+
+/* Reading data with limit */
+SELECT TOP number|percent column_name(s) FROM table_name WHERE condition;
+
+/* Deleting table */
+DELETE TABLE table_name;
+
